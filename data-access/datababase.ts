f5750c86e9f8f000d7ec9c4cfd7c08caa6eda4ba
Erick Valdivia <erick_valdivia@epam.com>
2020-11-04 09:02:18 -0600

    Added connection string into env

--- data-access/datababase.ts
+++ data-access/datababase.ts
@@ -1,6 +1,10 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('coursedb', 'postgres', 'admin', {
+const db = process.env.DB || '';
+const username = process.env.DB_USERNAME || '';
+const password = process.env.DB_PASSWORD || '';
+
+export const sequelize = new Sequelize(db, username, password, {
   host: 'localhost',
   dialect: 'postgres',
   pool: {
